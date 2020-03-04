be0fdb93d8b3e4219069d2cae910b54a7111e78c
jmmmnm <jmmmnm@gmail.com>
2020-03-04 19:09:09 +0900

    ボタン

---
+++ app/src/main/java/jp/gr/java_conf/jommomi/tsukattaapp/InputActivity.kt
@@ -0,0 +1,12 @@
+package jp.gr.java_conf.jommomi.tsukattaapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class InputActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_input)
+    }
+}
